7728492d63cadbd436b11bbd7e7ea398a1146eb2
Daniil Ivanov <dania-spb@mail.ru>
2018-10-01 21:27:17 +0200

    Lab1_tp_ver1.0

---
+++ lab1/src/main/train.cpp
@@ -0,0 +1,11 @@
+//
+// Created by daniil on 01.10.18.
+//
+
+#include "train.h"
+#include <iostream>
+#include <string>
+
+Train::Train(string, int, string) {}
+Train::Train() {}
+Train::~Train() {}
---
+++ lab1/src/main/train.h
@@ -0,0 +1,32 @@
+//
+// Created by daniil on 01.10.18.
+//
+
+#ifndef UNTITLED_TRAIN_H
+#define UNTITLED_TRAIN_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+/**
+ * Class Train.
+ * @author: Ivanov Daniil (dania-spb@mail.ru)
+ * @since: 1.10.2018
+ *
+ */
+class Train {
+    //contains destination's name
+    string destination;
+    //contains train's number
+    int trainNumber;
+    //contains departure's time
+    string departureTime;
+public:
+    Train(string, int, string);
+    Train();
+    ~Train();
+};
+
+
+#endif //UNTITLED_TRAIN_H
